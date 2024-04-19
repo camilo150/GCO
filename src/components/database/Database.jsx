e26edd6013d321a0b97527b16fa384b7b8de1284
camicastrillon2314@gmail.com <camicastrillon2314@gmail.com>
2024-04-19 15:44:42 -0500

    creando un nuevo objeto

---
+++ src/components/database/Database.jsx
@@ -0,0 +1,132 @@
+export const Database = () => {
+ return[
+    {
+      nombre: 'buzo amarillo',
+      marca: "Chevignon",
+      TipoPrenda: 'buzo amarillo chevignon',
+      Precio:"26000",
+      Sexo: 'hombre',
+      imagenUrl:"src/assets/img/prendas_Chevignon/buzo_Amarillo_Chevignon.jpeg"
+    },
+{
+  nombre: 'buzo azul',
+  marca: "Chevignon",
+  TipoPrenda: 'buzo azul chevignon',
+  Precio:"27100",
+  Sexo: 'hombre',
+  imagenUrl:'src/assets/img/prendas_Chevignon/buzo_Azul_chevignon.jpg',
+},
+{
+  nombre: 'buzo blanco',
+  marca: "Chevignon",
+  TipoPrenda: 'buzo blanco chevignon',
+  Precio:"36210",
+  Sexo:'mujer',
+  imagenUrl:'src/assets/img/prendas_Chevignon/buzo_Mujer_blanco_chevignon.jpg',
+},
+{
+  nombre: 'buzo',
+  marca: "Chevignon",
+  TipoPrenda: 'buzo chevignon',
+  Precio:"36123",
+  Sexo: 'mujer',
+  imagenUrl:'src/assets/img/prendas_Chevignon/buzo_Mujer_chevignon.jpg',
+},
+{
+  nombre: 'camisa negra',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa negra chevignon',
+  Precio:"32200",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camisa_negro_gris.jpg'
+},
+{
+  nombre: 'camisa blanca',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa blanca chevignon',
+  Precio:"31000",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camisaBlanca_chevignon.jpg'
+},
+{
+  nombre: 'camisa blanca',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa blanca chevignon',
+  Precio:"31550",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camisaBlancaML_chevignon.jpg'
+},
+{
+  nombre: 'camisa negra',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa negra chevignon',
+  Precio:"30050",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camisaNegraML_chevignon.jpg',
+},
+{
+  nombre: 'camisa roja',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa roja chevignon',
+  Precio:"30150",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camisaRojacuadro_chevignon.jpg'
+},
+{
+  nombre: 'camisa blanca',
+  marca: "Chevignon",
+  TipoPrenda: 'camisa blanca chevignon',
+  Precio:"30154",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/camiseta_Blanca_chevignon.jpeg'
+},
+{
+  nombre: 'chaqueta azul',
+  marca: "Chevignon",
+  TipoPrenda: 'chaqueta azul chevignon',
+  Precio:"40154",
+  Sexo: 'mujer',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/chaqueta_Chevignon.jpg'
+},
+{
+  nombre: 'jean azul',
+  marca: "Chevignon",
+  TipoPrenda: 'jean azul chevignon',
+  Precio:"31154",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/jean_AzulClaro_chevignon.jpg'
+},
+{
+  nombre: 'jean blanco',
+  marca: "Chevignon",
+  TipoPrenda: 'jean blanco chevignon',
+  Precio:"33254",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/jean_Blanco_chevignon.jpeg'
+},
+{
+  nombre: 'jean negro',
+  marca: "Chevignon",
+  TipoPrenda: 'jean negro chevignon',
+  Precio:"33254",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/jean_Negro_chevignon.png'
+},
+{
+  nombre: 'zapato azul',
+  marca: "Chevignon",
+  TipoPrenda: 'zapato azul chevignon',
+  Precio:"23254",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/zapato_azul_chevignon.jpg'
+},
+{
+  nombre: 'zapato blanco',
+  marca: "Chevignon",
+  TipoPrenda: 'zapato blanco chevignon',
+  Precio:"21214",
+  Sexo: 'hombre',
+  imagenUrl: 'src/assets/img/prendas_Chevignon/zapato_blanco_chevignon.jpg'
+},
+]
+}
