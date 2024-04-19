bc66aa42d7b1a2c62a9d10d7c7b9aa26ce8bfb06
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-19 14:36:40 -0500

    objetos americanino

--- src/components/database/Database.jsx
+++ src/components/database/Database.jsx
@@ -1,11 +1,175 @@
-const Database = () => {
- 
-    const ropa = {
-      nombre: 'Juan',
-      marca: 30,
-      TipoPrenda: 'Ciudad de Ejemplo',
-      Precio:"",
-      Sexo:"",
-      imagenUrl:""
-    };
+
+export const Database = () => {
+  return [
+  {
+    nombre: 'camisa verde lima',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:50000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/blusa_verde_Lima_mujer_americanino.jpg"
+  } ,
+  {
+    nombre: 'camiseta roja',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:70000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camisa-roja_mujer_americanino.jpg",
+  },
+  {
+    nombre: 'camiseta negra',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:70000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camisa_negra_americanino.jpg"
+  },
+  {
+    nombre: 'camiseta negra',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:70000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camisa_verde_lima_mujer_americanino.jpg"
+  },
+  {
+    nombre: 'camiseta verde militar',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:80000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta-verde-militar_americanino.webp"
+  },
+  {
+    nombre: 'camiseta blanca',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:80000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_blanca_americanino.webp"
+  },
+  {
+    nombre: 'camiseta blanca',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:80000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_blanca_mujer_americanino.jpg"
+  },
+  {
+    nombre: 'camisa gris',
+    marca: "Americanino",
+    TipoPrenda: 'camiseta',
+    Precio:40000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_gris_americanino.webp"
+  },
+  {
+    nombre: 'camiseta blanca',
+    marca: "Americanino",
+    TipoPrenda: 'camisa',
+    Precio:80000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_blanca_americanino.webp"
+  
+  },
+  {
+    nombre: 'camiseta naranga',
+    marca: "Americanino",
+    TipoPrenda: 'camiseta',
+    Precio:40000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_naranja_americanino.webp"
+  },
+  {
+    nombre: 'camiseta negra',
+    marca: "Americanino",
+    TipoPrenda: 'camiseta',
+    Precio:40000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/camiseta_negra_americanino.webp"
+  },
+  {
+    nombre: 'gorra blanca',
+    marca: "Americanino",
+    TipoPrenda: 'gorra',
+    Precio:50000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/gorra_blanca_americanino.jpg"
+  },
+  {
+    nombre: 'gorra de caminonero azul',
+    marca: "Americanino",
+    TipoPrenda: 'gorra',
+    Precio:50000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/gorra_camionero_azul_americanino.jpg"
+  },
+  {
+    nombre: 'gorra deportiva negra',
+    marca: "Americanino",
+    TipoPrenda: 'gorra',
+    Precio:60000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/gorra_deportiva_negra_americanino.jpg"
+  },
+  {
+    nombre: 'gorra negra',
+    marca: "Americanino",
+    TipoPrenda: 'gorra',
+    Precio:50000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/gorra_negra_americanino.jpg" 
+  
+  },
+  {
+    nombre: 'gorra rosa',
+    marca: "Americanino",
+    TipoPrenda: 'gorra',
+    Precio:40000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/gorra_rosa_americanio.jpg" 
+  },
+  {
+    nombre: 'medias azules',
+    marca: "Americanino",
+    TipoPrenda: 'medias',
+    Precio:20000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/medias_azules_americanino.jpg" 
+    },
+    {
+  nombre: 'medias rosas',
+  marca: "Americanino",
+  TipoPrenda: 'medias',
+  Precio:10000,
+  Sexo:"Femenino",
+  imagenUrl:"/src/assets/img/prendas_americanino/medias_rosa_americanino.jpg" },
+  {
+  nombre: 'medias vanilla',
+  marca: "Americanino",
+  TipoPrenda: 'medias',
+  Precio:20000,
+  Sexo:"Femenino",
+  imagenUrl:"/src/assets/img/prendas_americanino/medias_vainilla_americanino.jpg"
+  },
+  {
+    nombre: 'medias vino tinto',
+    marca: "Americanino",
+    TipoPrenda: 'medias',
+    Precio:20000,
+    Sexo:"Femenino",
+    imagenUrl:"/src/assets/img/prendas_americanino/medias_vinotinto_americanino.jpg"  
+  },
+  {
+    nombre: 'medias tobilleras vino tinto',
+    marca: "Americanino",
+    TipoPrenda: 'medias',
+    Precio:10000,
+    Sexo:"Masculino",
+    imagenUrl:"/src/assets/img/prendas_americanino/tobillera_vinotinto_americanino.jpg"
+  } 
+  ]
 }
+ 
