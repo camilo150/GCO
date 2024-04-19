aaf594b21fd4ff700382a7ea84a40a70126fb190
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-19 13:26:39 -0500

    se crea ChevignonData

--- src/components/database/Database.jsx
+++ src/components/database/Database.jsx
@@ -1,11 +1,22 @@
 const NafNafData = () => {
  
     const ropa = {
-      nombre: 'Juan',
+      nombre: 'Juana',
       marca: 30,
       TipoPrenda: 'Abrigo',
       Precio:"250000",
       Sexo:"Femenino",
       imagenUrl:"src/assets/img/prendas_nafnaf/abrigo_azul_claro_mujer_nafna.png"
     };
+}
+
+const ChevignonData = () => {
+  const ropa = {
+    nombre: 'Juan',
+    marca: 28,
+    TipoPrenda: 'Buzo',
+    Precio:"250000",
+    Sexo:"Masculino",
+    imagenUrl:"src/assets/img/prendas_nafnaf/abrigo_azul_claro_mujer_nafna.png"
+  };
 }
