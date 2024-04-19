e7c51363746ad53e2ffb6aec6052917b17f24874
Edwardlopez <152225209+EdwardLope@users.noreply.github.com>
2024-04-19 13:14:46 -0500

    se crea NafNafData

---
+++ src/components/database/Database.jsx
@@ -0,0 +1,11 @@
+const NafNafData = () => {
+ 
+    const ropa = {
+      nombre: 'Juan',
+      marca: 30,
+      TipoPrenda: 'Abrigo',
+      Precio:"250000",
+      Sexo:"Femenino",
+      imagenUrl:"src/assets/img/prendas_nafnaf/abrigo_azul_claro_mujer_nafna.png"
+    };
+}
